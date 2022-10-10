6e5be46684c80425f730f1d092f1f7bf3be91b8d
Sergey <Borozenniy.sergey@gmail.com>
2022-10-11 02:23:04 +0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ function App() {
 		<div className="App">
 			<Header />
 			<button onClick={onToggleButton}>Tиць</button>
-			<ProductList />
 			<Routes>
 				<Route index element={<ProductList />} />
 				<Route path={'form'} element={<Form />} />
